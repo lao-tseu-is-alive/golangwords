6601b479d325cccb88c11007db1a9e1287d6f393
CGil <lao.tseu.is.alive@gmail.com>
2018-07-19 14:46:35 +0200

    added wordcount functions

--- main.go
+++ main.go
@@ -10,29 +10,45 @@ import (
 	"unicode/utf8"
 	"os"
 	"strings"
+	"regexp"
+	"sort"
 )
-
-func add(a , b int) int {
-	return a + b
-}
-
-func addf(a , b float64) float64{
-	return a + b
-}
 
 func swap(s1, s2 string) (string, string) {
 	return s2, s1
 }
 
+// WordCount returns a map of the counts of each “word” in the string s.
+// but does not work at all with large utf 8 text and punctuation
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	counts := make(map[string]int, len(words))
+	for _, word := range words {
+		counts[word]++
+	}
+	return counts
+}
+
+// returns a map of the counts of each word using correct utf8 separation
+func WordCount2(text string) map[string]int {
+	reWords := regexp.MustCompile("\\p{L}+")
+	words := reWords.FindAllString(text, -1)
+	counts := make(map[string]int, len(words))
+	for _, word := range words {
+		counts[word]++
+	}
+	return counts
+}
+
 func reverseUtfWord(word string) string {
-	var reversed  string
+	var reversed string
 	i := 0
 	for len(word) > 0 {
 		runeValue, size := utf8.DecodeLastRuneInString(word)
 		i += size
-		fmt.Printf("negative index=[%v] char[%q]\n",i, runeValue)
+		fmt.Printf("negative index=[%v] char[%q]\n", i, runeValue)
 		reversed += string(runeValue)
-		word = word[:len(word) - size]
+		word = word[:len(word)-size]
 	}
 	return reversed
 }
@@ -45,7 +61,7 @@ func sayHello(name string) {
 	fmt.Println(swap("Hello", "World"))
 }
 
-func nextWeekend()  {
+func nextWeekend() {
 	today := time.Now().Weekday()
 	fmt.Printf("Today is %v, %v\n", today, time.Now().Format(time.RFC3339))
 	switch time.Saturday {
@@ -57,7 +73,6 @@ func nextWeekend()  {
 		fmt.Println("In two days it's week-end !")
 	default:
 		fmt.Println("Week-end is far away ...")
-
 
 	}
 }
@@ -93,14 +108,34 @@ func readTextFile(filename string) string {
 	return string(buffer)
 }
 
-func main()  {
+func main() {
 	// picked a 
 	const inputFilename string = "./data/LES MISÉRABLES -- Tome I -- FANTINE.utf8_txt"
 	var text = readTextFile(inputFilename)
 	lines := strings.Split(text, "\n")
 	fmt.Printf("The text is %v bytes long\n", len(text))
-	fmt.Printf("and have %v lines\n", len(lines))
-	fmt.Printf(lines[45])
+	fmt.Printf("and have %v lines\n\n\n", len(lines))
+
+
+	words1 :=WordCount(text)
+	fmt.Println(words1)
+	sortedWords1 := make([]string, 0, len(words1))
+	for word := range words1 {
+		sortedWords1 = append(sortedWords1, word)
+	}
+	sort.Strings(sortedWords1) //sort by key
+	fmt.Println(sortedWords1)
+	fmt.Println(len(words1))
+
+	words2 := WordCount2(text)
+	fmt.Println(words2)
+	sortedWords2 := make([]string, 0, len(words2))
+	for word := range words2 {
+		sortedWords2 = append(sortedWords2, word)
+	}
+	sort.Strings(sortedWords2) //sort by key
+	fmt.Println(sortedWords2)
+	fmt.Println(len(words2), len(sortedWords2))
+
 
 }
-
